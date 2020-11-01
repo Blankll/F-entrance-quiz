2fd21784590050003ef5cfe023a8f017221a7982
kevindongzg <dongkevin94@gmail.com>
2020-11-01 16:57:47 +0800

    add TODOs

--- src/App/App.js
+++ src/App/App.js
@@ -9,7 +9,7 @@ import HeroIndex from '../pages/HeroIndex/HeroIndex';
 // TODO GTB-3: * 未使用语义化标签
 // TODO GTB-3: * 使用了Flex布局，但没有使用scss
 // TODO GTB-3: * 运用了许多ES6+语法及axios，很新的语法也看到有使用到
-// TODO GTB-4: * 有小步提交，message可以更加明确具体
+// TODO GTB-4: * 有小步提交，提交可以按照组件或页面构成再进一步拆分
 // TODO GTB-4: * 有抽出utils与api请求层
 // TODO GTB-4: * 组件抽象复用有待加强，部分组件可以合并，注意命名（需求是学员，不是hero）
 // TODO GTB-4: * 有划分comonents与pages
